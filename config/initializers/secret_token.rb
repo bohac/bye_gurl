c8a223f8b08cc5b7edce1eef785fd9739374532e
A.J. Bohac <ajbohac@gmail.com>
2012-01-08 19:16:09 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ByeGurl::Application.config.secret_token = 'a3cccbede062843fd8441bb71f912b059e855f95e923bcd373ab982090218cc97a65ef0d43ac05f68d9a0c80868eeceb35525b331454e0b3a2b669f99daed2a7'
